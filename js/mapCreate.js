68b85db89acd8ac67e07504a3c896cfc5c829455
DESKTOP-LHMLSNM\손명준 <sonbill971@gmail.com>
2018-07-10 23:57:24 +0900

    tooltip created

---
+++ js/mapCreate.js
@@ -0,0 +1,107 @@
+var position;
+var audio = new Audio('./source/sound/ui-sound-19.ogg');
+
+function getAbsolutePos(obj) {
+    var position = new Object;
+    position.x = 0;
+    position.y = 0;
+    if (obj) {
+        position.x = obj.offsetLeft + obj.clientLeft;
+        position.y = obj.offsetTop + obj.clientTop;
+        if (obj.offsetParent) {
+            var parentpos = getAbsolutePos(obj.offsetParent);
+            position.x += parentpos.x;
+            position.y += parentpos.y;
+        }
+    }
+    return position;
+}
+function createTooltip(cursorX, cursorY){
+    // var tooltip = document.createElement("div");
+    // tooltip.setAttribute("id","tooltip");
+    // tooltip.setAttribute("position","absolute");
+    // tooltip.style.left=(cursorX-64)+"px";
+    // tooltip.style.top=(cursorY-64)+"px";
+    // document.getElementById("#wrapper").appendChild(tooltip);
+    $("#tooltip").css({
+        "display":"block",
+        "top":cursorY,
+        "left":cursorX,
+        "position": "absolute"}).show();
+}
+function removeTooltip()
+{
+    $("#tooltip").css({
+        "display":"none"});
+        console.log("removeTooltip");
+}
+var parentWidth = $('#map').width();
+var parentHeight = $('#map').height();
+var canvas = d3.select("#map").append("svg")
+    .attr("id", "svg")
+    .attr("width", parentWidth)
+    .attr("height", parentHeight);
+
+d3.selection.prototype.moveToFront = function() {
+    return this.each(function() {
+        console.log(this.parentNode.firstChild.id)
+        this.parentNode.parentNode.appendChild(this.parentNode);
+    });
+};
+d3.selection.prototype.moveToBack = function() {
+    return this.each(function() {
+        var firstChild = this.parentNode.parentNode.firstChild;
+        if (firstChild) {
+            this.parentNode.parentNode.insertBefore(this.parentNode, firstChild);
+        }
+    });
+};
+d3.json("./mp.geojson", function(data) {
+    var group = canvas.selectAll("g")
+        .data(data.features)
+        .enter()
+        .append("g");
+    var projection = d3.geo.mercator()
+        .center([127.5, 36])
+        .scale(3900)
+        .translate([parentWidth/2, parentHeight/2]);
+    var path = d3.geo.path().projection(projection);
+
+    var areas = group.append("path")
+        .attr("d", path)
+        .attr("stroke", "blue")
+        .attr("Stroke-width", 10)
+        .attr("fill", "black")
+        .attr("class", "path")
+        .attr("id", function(d) {
+            return d.properties.name_eng;
+        })
+        .attr("opacity", "1")
+        .on("mouseover", function(d) {
+            audio.pause();
+            audio.currentTime = 0;
+            audio.play();
+            position = getAbsolutePos(d3.select(this));
+            d3.select(this).transition()
+                .attr("transform", null)
+                .attr("transform", " translate(0,-10)")
+                .duration(400).style("fill", "#eeefff")
+            d3.select(this).moveToFront();
+            var cursorX = event.pageX;
+            var cursorY = event.pageY;
+            createTooltip(cursorX,cursorY);
+        })
+        .on("mouseout", function() {
+            d3.select(this).transition()
+                .duration(300)
+                .style("fill", "black")
+                .attr("transform", "translate(0,0)")
+                .delay(100);
+            if (getAbsolutePos(d3.select(this)) == position) {
+                d3.select(this).moveToBack();
+            }
+            removeTooltip();
+        });
+
+})
+console.log("mapcreated");
